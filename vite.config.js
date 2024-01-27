74323fcda57c7613cbce08e577fbbf875fcb1509
soava <soavamahary@gmail.com>
2024-01-27 18:23:31 +0300

    modif

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/soava-portfolio/",
+  base: "/react-pages/",
   plugins: [react()],
 })
