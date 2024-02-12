20011dcbfdee6f24b5283cb4839437699e653d7e
soava <soavamahary@gmail.com>
2024-02-12 22:06:29 +0300

    commit homepage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ function ColorSchemesExample() {
         expand='sm'
       >
         <Container>
-          <Navbar.Brand className='brand' href="#home"> {"</Soava>"} </Navbar.Brand>
+          <Navbar.Brand className='brand' href="https://soavanyanjara.github.io/react-pages/"> {"</Soava>"} </Navbar.Brand>
           <Navbar.Toggle className="justify-content-center" aria-controls="basic-navbar-nav" />
           <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav">
             <Nav className="justify-content-center">
